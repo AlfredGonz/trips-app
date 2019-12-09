d573c66820ce893236095450d33d8246487d6946
Alfredo Gonzalez <manuel.martinez96@hotmail.com>
2019-12-09 10:08:05 -0600

    pasando parametros

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
 
 class DescriptionPlace extends StatelessWidget {
-  final descriptionDummy =
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-      "\n\n "
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+
+  String namePlace;
+  int Starts;
+  String descriptionPlace;
+
+
+  DescriptionPlace(this.namePlace, this.Starts, this.descriptionPlace);
+
   @override
   Widget build(BuildContext context) {
+
+    final star_half = Container(
+      margin: EdgeInsets.only(top: 323.0, right: 3.0),
+      child: Icon(
+        Icons.star_half,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final star_border = Container(
+      margin: EdgeInsets.only(top: 323.0, right: 3.0),
+      child: Icon(
+        Icons.star_border,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
     final star = Container(
       margin: EdgeInsets.only(top: 323.0, right: 3.0),
       child: Icon(
@@ -18,7 +39,7 @@ class DescriptionPlace extends StatelessWidget {
     final description = Container(
       margin: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
       child: Text(
-        descriptionDummy,
+        descriptionPlace,
         style: const TextStyle(
             fontSize: 16.0,
             fontWeight: FontWeight.bold,
@@ -34,7 +55,7 @@ class DescriptionPlace extends StatelessWidget {
             left: 20.0,
             right: 20.0,
           ),
-          child: Text('Duwili Ella',
+          child: Text(namePlace,
               style: TextStyle(
                 fontSize: 30.0,
                 fontWeight: FontWeight.w900,
@@ -47,7 +68,7 @@ class DescriptionPlace extends StatelessWidget {
             star,
             star,
             star,
-            star,
+            star_border,
           ],
         )
       ],
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,11 @@ import 'package:trips_app/description_place.dart';
 void main() => runApp(App());
 
 class App extends StatelessWidget {
+
+  final descriptionDummy =
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      "\n\n "
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +18,7 @@ class App extends StatelessWidget {
         appBar: AppBar(
     title: Text('Trips App')
     ),
-    body: DescriptionPlace(),
+    body: DescriptionPlace("Bahamas", 4, descriptionDummy ),
     )
     );
   }
