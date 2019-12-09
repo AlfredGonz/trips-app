323345447bb6396e4a80792f57ca28705293ae3c
Alfredo Gonzalez <manuel.martinez96@hotmail.com>
2019-12-09 10:21:32 -0600

    Agegando fuente Lato

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -41,6 +41,7 @@ class DescriptionPlace extends StatelessWidget {
       child: Text(
         descriptionPlace,
         style: const TextStyle(
+          fontFamily: 'Lato',
             fontSize: 16.0,
             fontWeight: FontWeight.bold,
             color: Color(0xFF56575a)),
@@ -57,6 +58,7 @@ class DescriptionPlace extends StatelessWidget {
           ),
           child: Text(namePlace,
               style: TextStyle(
+                fontFamily: 'Lato',
                 fontSize: 30.0,
                 fontWeight: FontWeight.w900,
               ),
