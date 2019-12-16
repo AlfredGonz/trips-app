87a39b1789612bf0144ccefd0838c9d35c6486b4
Alfredo Gonzalez <manuel.martinez96@hotmail.com>
2019-12-16 16:45:36 -0600

    agregando lista de imagenes a appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:trips_app/description_place.dart';
-import 'package:trips_app/gradient_back.dart';
+import 'package:trips_app/header_appbar.dart';
 import 'package:trips_app/review_list.dart';
 
 void main() => runApp(App());
 
 class App extends StatelessWidget {
-
   final descriptionDummy =
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
       "\n\n "
@@ -14,24 +13,21 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Trips App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
-      home: Scaffold(
-        body: Stack(
-          children: <Widget>[
-            ListView(
-              children: <Widget>[
-                DescriptionPlace("Bahamas", 4, descriptionDummy ),
-                ReviewList()
-              ],
-            ),
-            GradientBack('Popular'),
-          ],
-        ),
-    )
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Trips App',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: Scaffold(
+          body: Stack(
+            children: <Widget>[
+              ListView(
+                children: <Widget>[
+                  DescriptionPlace("Bahamas", 4, descriptionDummy),
+                  ReviewList()
+                ],
+              ),
+              HeaderAppBar(),
+            ],
+          ),
+        ));
   }
 }
