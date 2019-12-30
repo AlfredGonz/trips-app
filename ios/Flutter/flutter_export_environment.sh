7d9ac0ceba732036a4f2af2ee7192c2cd98387b5
Alfredo <manuel.martinez96@hotmail.com>
2019-12-30 16:14:08 -0600

    creando floating action button verde

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\ALFREDO\Documents\flutterApps\trips_app"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/sgb/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/sgb/Documents/flutter-Apps/trips-app"
+export "FLUTTER_TARGET=/Users/sgb/Documents/flutter-Apps/trips-app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/sgb/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
