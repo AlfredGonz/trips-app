241e352c5a20a6f80f4636fa517ed19699f5c187
Alfredo <manuel.martinez96@hotmail.com>
2020-01-09 15:11:40 -0600

    generando navegacion en bottom navigation bar

--- lib/al_trips.dart
+++ lib/al_trips.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:trips_app/home_trips.dart';
+import 'package:trips_app/profile_trips.dart';
+import 'package:trips_app/search_trips.dart';
 
 class AlTrips extends StatefulWidget {
   @override
@@ -6,16 +9,32 @@ class AlTrips extends StatefulWidget {
 }
 
 class _AlTripsState extends State<AlTrips> {
+  int index = 0;
+  final List<Widget> widgetsChildren = [
+    HomeTrips(),
+    SearchTrips(),
+    ProfileTrips()
+  ];
+
+  void onTapTapped (int indextap) {
+        setState(() {
+          index = indextap;
+        });
+    }
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
+      body: widgetsChildren[index],
       bottomNavigationBar: Theme(
         data: Theme.of(context).copyWith(
           canvasColor: Colors.white,
           primaryColor: Colors.purple
         ),
-        child: BottomNavigationBar(items: [
+        child: BottomNavigationBar(
+          currentIndex: index,
+          onTap: onTapTapped,
+          items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             title: Text('')
